fb526952653ba719de36f1cd01e79ec8304af10e
jaglyder <98897498+jaglyder@users.noreply.github.com>
2022-02-02 18:42:11 -0500

    CIT 204
    A very basic web page

---
+++ Website/JS1.js
@@ -0,0 +1,21 @@
+var canvas = document.querySelector('canvas')
+	;
+	
+canvas.width = 1000;
+canvas.height = 600;
+
+var c = canvas.getContext('2d');
+
+console.log(canvas);
+
+for(var i = 0; i < 100; i++){
+    var x = Math.random() * 1000;
+    var y = Math.random() * 600;
+    var r = Math.random() * 50;
+	var color = ['cyan', 'chartreuse', 'coral', 'fuchsia', 'HotPink', 'ivory', 'grey', 'aliceblue', 'aqua', 'black', 'blueviolet', 'lightgreen', 'orange', 'Salmon', 'purple', 'lightpink', 'mediumspringgreen'];
+    var strokeStyle = color[Math.floor(Math.random() * color.length)];
+    c.beginPath();
+    c.arc(x, y, r, 0, Math.PI * 2, false);
+    c.strokeStyle = strokeStyle;
+    c.stroke();
+}
